b256a4553dc9c30c25c508ae5c33c3e7f87b7197
kar-lina <akarabutova59@gmail.com>
2024-08-07 11:53:38 +0300

    Add menu

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import 'scss/style'
 import { fileLoaderInit } from './js/load-file'
 import { dropdownMenuInit } from './js/dropdown'
+import { menuInit } from './js/menu'
 
 document.addEventListener('DOMContentLoaded', function () {
   fileLoaderInit()
   dropdownMenuInit()
+  menuInit()
 })
